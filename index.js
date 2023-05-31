3f112356eb810090683fef6dc23c6e199bff2e90
Juan Cardenas <cardenasriosjuandavid@gmail.com>
2023-05-31 07:15:41 -0500

    new changes for navigation and script

---
+++ index.js
@@ -0,0 +1,20 @@
+function login(nombre) {
+  // alert('Felicitaciones haz dado click al boton: ' + nombre)
+
+  const valueConfirm = confirm("BOTON DE CONFIRMACIÓN");
+
+  let messageResult = "";
+
+  if (valueConfirm) {
+    messageResult = "verdadero";
+  } else {
+    messageResult = "falso";
+  }
+
+  console.log("El resultado fue: " + messageResult);
+}
+
+
+const exampleArrays = () => {
+    
+}
